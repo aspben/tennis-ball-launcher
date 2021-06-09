a16c1ff03fb6be392319ce34ee06b9b94035c40e
BENOIT ASPIRAULT <benoit.aspirault@gmail.com>
2021-06-09 14:18:25 -0400

    change before overhaul

---
+++ arduinoCode/remoteArduinoServoControler/remoteArduinoServoControler.ino
@@ -0,0 +1,145 @@
+/*
+  LED
+
+  This example creates a BLE peripheral with service that contains a
+  characteristic to control an LED.
+
+  The circuit:
+  - Arduino MKR WiFi 1010, Arduino Uno WiFi Rev2 board, Arduino Nano 33 IoT,
+    Arduino Nano 33 BLE, or Arduino Nano 33 BLE Sense board.
+
+  You can use a generic BLE central app, like LightBlue (iOS and Android) or
+  nRF Connect (Android), to interact with the services and characteristics
+  created in this sketch.
+
+  This example code is in the public domain. */
+
+
+#include <ArduinoBLE.h>
+
+BLEService launcherService("ed24d43a-a623-11eb-bcbc-0242ac130002"); // BLE LED Service
+
+// BLE Feed characteristic (servo on pin D2) - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic feedCharacteristic("febe13d2-a623-11eb-bcbc-0242ac130002", BLERead | BLEWrite);
+
+// BLE Speed characteristic (servo on pin D3) - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic speedCharacteristic("0c60be4a-a624-11eb-bcbc-0242ac130002", BLERead | BLEWrite);
+
+// BLE Spin characteristic (servo on pin D5) - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic spinCharacteristic("1216429c-a624-11eb-bcbc-0242ac130002", BLERead | BLEWrite);
+
+// BLE Spin characteristic (servo on pin D5) - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic oscillateCharacteristic("ac9fbcb2-ac32-11eb-8529-0242ac130003", BLERead | BLEWrite);
+
+
+#include <Servo.h>
+Servo feedServo;
+Servo speedServo;
+Servo spinServo;
+
+
+void setup() {
+  Serial.begin(9600); 
+
+ feedServo.write(0);
+ feedServo.attach(2); 
+ delay(500);
+
+  
+ speedServo.write(0);
+ speedServo.attach(3);
+ delay(500);
+
+ spinServo.write(90);
+ spinServo.attach(5);
+
+ pinMode(6, OUTPUT);
+ 
+ 
+
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+    while (1);
+  }
+
+  // set advertised local name and service UUID:
+  BLE.setLocalName("BALLLAUNCHER");
+  BLE.setAdvertisedService(launcherService);
+
+  // add the characteristic to the service
+  launcherService.addCharacteristic(feedCharacteristic);
+  launcherService.addCharacteristic(speedCharacteristic);
+  launcherService.addCharacteristic(spinCharacteristic);
+  launcherService.addCharacteristic(oscillateCharacteristic);
+
+  // add service
+  BLE.addService(launcherService);
+
+  // set the initial value for the characeristic:
+  feedCharacteristic.writeValue(20);
+  speedCharacteristic.writeValue(10);
+  spinCharacteristic.writeValue(90);
+  oscillateCharacteristic.writeValue(0);
+
+
+  
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("BLE TennisBallLauncher Peripheral all set"); 
+}
+
+void loop() {
+  
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+
+    // while the central is still connected to peripheral:
+    while (central.connected()) {
+      // if the remote device wrote to the characteristic,      
+      
+      if (feedCharacteristic.written()) {
+        Serial.print("Change to feedCharacteristic");
+        Serial.println(feedCharacteristic.value());
+        feedServo.write(feedCharacteristic.value());
+      }
+
+       if (speedCharacteristic.written()) {
+          Serial.print("Change to speedCharacteristic");
+        Serial.println(speedCharacteristic.value());
+        speedServo.write(speedCharacteristic.value());
+      }
+
+       if (spinCharacteristic.written()) {
+        Serial.print("Change to spinCharacteristic");
+        Serial.println(spinCharacteristic.value());
+        spinServo.write(spinCharacteristic.value());
+      } 
+
+      if (oscillateCharacteristic.written()) {
+        Serial.print("Change to oscillateCharacteristic");
+        Serial.println(oscillateCharacteristic.value());
+        
+        if (oscillateCharacteristic.value() == 0 ) {
+          digitalWrite(6,LOW);
+        }
+          
+        if (oscillateCharacteristic.value() == 1) {
+          digitalWrite(6,HIGH);
+        }       
+      }
+
+    }
+
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  }
+}
--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,45 +1,58 @@
 <template>
-  <h1>Ball launcher</h1>
-  <svg
-    v-if="!this.working"
-    @click="this.working = true"
-    xmlns="http://www.w3.org/2000/svg"
-    height="48"
-    viewBox="0 0 24 24"
-    width="48"
-  >
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M8 5v14l11-7z" />
-  </svg>
-  <svg
-    v-if="this.working"
-    @click="this.working = false"
-    xmlns="http://www.w3.org/2000/svg"
-    height="48"
-    viewBox="0 0 24 24"
-    width="48"
-  >
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
-  </svg>
+  <h1>Ball launcher</h1>   
 
-  <p>Vitesse = {{speed}}</p>
-  <p>Délais = {{delay}}</p>
-  <p>Spin = {{spin}}</p>
-  <p>characteristic: {{characteristic}}</p>
+  <div v-if="this.service">
+    <div class="slidecontainer">
+      <div class="launcherProperty">Feed : {{convertFeed(feedSpeed)}} {{feedSpeed}} </div>
+      <input type="range" min="0" max="180" v-model="feedSpeed" @change="editFeed(feedSpeed)" class="slider feedRange" id="myRange">
+    </div>
 
+    <div class="slidecontainer">
+      <div class="launcherProperty">Speed : {{ballSpeed}}</div>
+      <input type="range" min="0" max="180" v-model="ballSpeed" @change="editSpeed(ballSpeed)" class="slider oneToTen" id="myRange">
+    </div>
+
+    <div class="slidecontainer">
+      <div class="launcherProperty">Spin : {{spinValue}}</div>
+      <input type="range" min="0" max="180" v-model="spinValue" @change="editSpin(spinValue)" class="slider oneToTen" id="myRange">
+    </div>
+
+    <div class="slidecontainer onOffSlider">
+      <div class="launcherProperty">Oscillate : {{oscillateValue === "1" ? true : false}}</div>
+      <input type="range" min="0" max="1" step="1" v-model="oscillateValue" @change="editOscillate(oscillateValue)" class="slider trueFalse" id="myRange">
+    </div>
+  </div>
+  
+
+  <!--<div class="speech" @click="speechOn = !speechOn">
+    <button v-if="!speechOn" >
+      <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24"><path d="M19 11h-1.7c0 .74-.16 1.43-.43 2.05l1.23 1.23c.56-.98.9-2.09.9-3.28zm-4.02.17c0-.06.02-.11.02-.17V5c0-1.66-1.34-3-3-3S9 3.34 9 5v.18l5.98 5.99zM4.27 3L3 4.27l6.01 6.01V11c0 1.66 1.33 3 2.99 3 .22 0 .44-.03.65-.08l1.66 1.66c-.71.33-1.5.52-2.31.52-2.76 0-5.3-2.1-5.3-5.1H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c.91-.13 1.77-.45 2.54-.9L19.73 21 21 19.73 4.27 3z"/></svg>
+    </button>
+    <button v-else>
+      <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24"><path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z"/></svg>
+    </button>
   <div class="box">{{runtimeTranscription}}</div>
-  <p class="smallText">Dernière commande: {{sentences[0]}}</p>
+  </div>
+  
+  <p class="smallText">Dernière commande: {{sentences[0]}}</p>-->
 
-  <button @click="connectToLauncher">Connect Ball Launcher</button>
-  <button @click="startLed">Start</button>
-  <button @click="stopLed">Stop</button>
+  <button class="btn" @click="connectToLauncher">Connect Ball Launcher</button>
+  <div>
+    Server: {{server ? server.connected : null}} 
+
+  </div>
+  <div>
+    Service: {{JSON.stringify(service)}}
+  </div>
+
+
 </template>
 
 <script>
 let SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 let recognition = SpeechRecognition ? new SpeechRecognition() : false;
+
 
 export default {
   name: "OneAndOnly",
@@ -53,8 +66,14 @@ export default {
     speaking: false,
     sentences: [],
     characteristic: null,
+    device: null,
+    server: null,
     service: null,
-
+    feedSpeed: 0,
+    ballSpeed:0,
+    spinValue: 0,
+    oscillateValue: 0,
+    speechOn: false,
     speed: 5, // km/h
     delay: 2, // s entre chaque balle
     spin: 100, // RPM positif = topspin; négatif = backspin
@@ -63,48 +82,58 @@ export default {
 
   mounted() {
     this.checkCompatibility();
-    this.startSpeechRecognition();
+    //this.startSpeechRecognition();
   },
   methods: {
-    async connectToLauncher() {
-      let device = await navigator.bluetooth
-        .requestDevice({
-          filters: [
-            {
-              name: "LED",
-            },
-          ],
-          optionalServices: ["19b10000-e8f2-537e-4f6c-d104768a1214"]
-        })
-
-        let server = await device.gatt.connect()
-        this.service = await  server.getPrimaryService("19b10000-e8f2-537e-4f6c-d104768a1214");
+    convertFeed(feed) {
+      if( feed < 52 ) {
+        return "stop"
+      } else { 
+        // between 52 and 180 the feed pass from 1 per 8 sec to 1 per second
+        return `${Math.round((9 - ((feed - 52) / (180 - 52)  * 7 + 1))* 10) / 10} sec.`
         
+
+      }
+      
     },
 
-    async startLed () {
-      this.characteristic = await this.service.getCharacteristic("19b10001-e8f2-537e-4f6c-d104768a1214" );
-          var newValue = Uint8Array.of(1);
-        this.characteristic.writeValue(newValue);
-        console.log('green on')
+      connectToLauncher() {
 
-        this.characteristic = await this.service.getCharacteristic("19b10002-e8f2-537e-4f6c-d104768a1214" );
-          var newValue = Uint8Array.of(0);
-        this.characteristic.writeValue(newValue);
-        console.log('red Off')
+        navigator.bluetooth.requestDevice({
+        filters: [{ services: ['ed24d43a-a623-11eb-bcbc-0242ac130002'] }],
+      })
+        .then(device => device.gatt.connect())
+        .then(server => server.getPrimaryService('ed24d43a-a623-11eb-bcbc-0242ac130002'))
+        .then(service => this.service = service)
+        .then(() => {
+          this.editFeed(30)
+          this.editSpin(90)
+        })        
+        .catch(error => alert(`Oups error: ${error}`))       
+  },
+
+
+    async editFeed (newFeed) {
+      let characteristic = await this.service.getCharacteristic("febe13d2-a623-11eb-bcbc-0242ac130002");
+      await characteristic.writeValue(Uint8Array.of(newFeed));        
     },
 
-    async stopLed () {
-      this.characteristic = await this.service.getCharacteristic("19b10001-e8f2-537e-4f6c-d104768a1214" );
-          var newValue = Uint8Array.of(0);
-        this.characteristic.writeValue(newValue);
-        console.log('green Off')
-
-        this.characteristic = await this.service.getCharacteristic("19b10002-e8f2-537e-4f6c-d104768a1214" );
-          var newValue = Uint8Array.of(1);
-        this.characteristic.writeValue(newValue);
-        console.log('red On')
+    async editSpeed (newSpeed) {
+      let characteristic = await this.service.getCharacteristic("0c60be4a-a624-11eb-bcbc-0242ac130002");
+      await characteristic.writeValue(Uint8Array.of(newSpeed));
     },
+
+    async editSpin (newSpin) {
+        let characteristic = await this.service.getCharacteristic("1216429c-a624-11eb-bcbc-0242ac130002");
+      await characteristic.writeValue(Uint8Array.of(newSpin));
+    },
+
+    async editOscillate (oscillateValue) {      
+      let characteristic = await this.service.getCharacteristic("ac9fbcb2-ac32-11eb-8529-0242ac130003");
+      await characteristic.writeValue(Uint8Array.of(oscillateValue));
+    },
+
+   
 
     processCommand(command) {
       let commandArray = command.split(" ");
@@ -164,14 +193,14 @@ export default {
           "Speech Recognition is not available on this browser. Please use Chrome or Firefox";
         return false;
       }
-      console.log("startSpeechRecognition has started");
+      console.console.log("startSpeechRecognition has started");
       this.toggle = true;
       recognition.lang = this.lang;
       recognition.interimResults = true;
 
       recognition.addEventListener("speechstart", (event) => {
         this.speaking = true;
-        console.log("I heard something");
+        console.console.log("I heard something");
       });
 
       recognition.addEventListener("speechend", (event) => {
@@ -211,13 +240,261 @@ export default {
 
 
 <style>
+
+body {
+    /*Removes the default margin preset by the browser*/
+    margin: 0;
+    min-height: 100vh;
+    /* Used to vertically center <main> when <main>'s margin is set to auto */
+    background-color: #b4c1cb;
+    /* Image for texture and lightening effect on background */
+    background-image: 
+                linear-gradient(135deg, rgba(255,255,255,.5) 30%, rgba(0,0,0,.1)),
+                url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAANCAYAAACZ3F9/AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsIAAA7CARUoSoAAAAEnSURBVChTPZJJcoQwDEXlqfv+d8s2m9wCsEX+kxGmXIA1/EEuP79/t5aNMYx37mLFWqu23I3VWrO1ltXazJ13rVHEoqiUwqFVFd2uRtr8zzkjx8yVZ1ZJdnVlUxQhfdPdFft8P3EOIntO0NScRAKJRBHNQIHqvC47zzMagkpexFvrL8UsejhtvfpJXZzzDTFR3eKTInR679F5PZpLQbs0h3kwElVQ0hgC2cCXW1XxjkNvS9rSQJaWLATpdVbIjkFhfYtiULPx62qKT7PmvJRbRXsEwh4Djbb7u0WIxsVtOai8IcM3zl7a/sw08jMZKkmDZObIzGLGCgzJ6JoANscNSsS1ZiRlUc42kDHpuY4uV6Ffj+N4rxPJqSHoypQbV0UxDNRDE0byD7qr9Go2pueNAAAAAElFTkSuQmCC');
+                
+}
+
+main {
+    margin: auto;
+}
+
+input[type="range"] {
+    /* To remove default styles appplied by webkit browsers */
+    -webkit-appearance: none; 
+    width: 85vw;
+    background: transparent;
+    position: relative;
+}   
+
+input[type="range"].trueFalse {
+    /* To remove default styles appplied by webkit browsers */
+    -webkit-appearance: none; 
+    width: 30vw;
+    background: transparent;
+    position: relative;
+}   
+
+.slidecontainer {
+  margin: 40px 20px 40px 20px
+}
+
+.onOffSlider {
+  display: flex;
+  align-content: flex-start;
+  justify-content:space-around ;
+}
+
+.launcherProperty {
+  text-align: left;
+  font-size:large;
+  margin: 16px;
+}
+
+input[type="range"]:focus {
+    /* To remove default (mostly blue) outline appplied by browsers on :focus */
+    outline: 0;
+}
+
+/* SLIDER THUMB -> represented by the orange ball in the design */
+/* Webkit based browsers (chrome, opera, ...) */
+input[type="range"]::-webkit-slider-thumb {
+     /* Removes the default styling */
+    -webkit-appearance: none;
+
+    width: 3em;
+    height: 3em;
+    margin: .5em 0;
+    background: hsl(27, 98%, 50%);
+    border-radius: 100%;
+
+    /* The inset shadow gives depth and makes the thumb look 3 dimensional and the other one shows a subtle effect of the shadow it creates because of the light source coming from the top left of the page */
+    box-shadow: 
+                inset .8em .8em 5px -8px rgba(255, 255, 255, .4),
+                inset -.4em -.4em 10px -3px rgba(0,0,0,.2),
+                0.7em 0.5em 7px -0.5em rgba(0,0,0,.4);;
+
+    cursor: pointer;
+}
+
+/*All the same stuff for firefox */
+input[type=range]::-moz-range-thumb {
+    width: 3em;
+    height: 3em;
+    margin: .5em 0;
+    background: #fd8421;
+    border-radius: 100%;
+    box-shadow: 
+                inset .8em .8em 5px -8px rgba(255, 255, 255, .4),
+                inset -.4em -.4em 10px -3px rgba(0,0,0,.2),
+                0.7em 0.5em 7px -0.5em rgba(0,0,0,.4);
+    cursor: pointer;
+    border: 0;
+}
+
+
+/* RUNNABLE TRACK -> represented by the indented carve in which the ball (thumb) moves */
+/* Webkit browsers */
+input[type="range"]::-webkit-slider-runnable-track {
+    background: transparent;
+    width: 100vw;
+    height: 4em;
+    padding: 0 1em;
+    border-radius: 2em;
+
+    /* These shadow are what create the skeumorphic indent on the track. The first one is dark to show places in the indent that light don't reach and the second one shows places where light meets thereby creating the illusion of depth */ 
+    box-shadow: 
+                inset .6em 1em 10px rgba(0,0,0,.15),
+                inset -.6em -1em 10px rgba(255, 255, 255, .415);
+}
+/* For firefox */
+input[type=range]::-moz-range-track {
+    background: transparent;
+    width: 100vw;
+    height: 4em;
+    padding: 0 1em;
+    border-radius: 2em;
+    box-shadow: 
+                inset .6em 1em 10px rgba(0,0,0,.15),
+                inset -.6em -1em 10px rgba(255, 255, 255, .415);
+}
+
+/* For the numbering in the range's design - doesn't show on firefox but shows on chrome and opera */
+input[type="range"].oneToTen::before {
+    content: "0 1 2 3 4 5 6 7 8 9";
+    position: absolute;
+    text-align: center;
+    color: #c4c4c4;
+    font-size: 1em;
+
+    /*Serves to spread the letters to fit the whole width of the track */
+    letter-spacing: 2.5vw;
+
+    top: 0; bottom: 0;
+    left: 0; right: 0;
+    padding: 1.6em 0.6em 1.6em 0.6em;
+
+    /* Makes the letters look embossed adding to the skeumorphic theme of the design */
+    text-shadow: .5px .5px 0.9px rgba(0,0,0, .4);
+
+    z-index: -1;
+}
+
+/* For the numbering in the range's design - doesn't show on firefox but shows on chrome and opera */
+input[type="range"].feedRange::before {
+    content: "stop 1 2 3 4 5 6 7 8";
+    position: absolute;
+    text-align: center;
+    color: #c4c4c4;
+    font-size: 1em;
+
+    /*Serves to spread the letters to fit the whole width of the track */
+    letter-spacing: 2.5vw;
+
+    top: 0; bottom: 0;
+    left: 0; right: 0;
+    padding: 1.6em 0.6em 1.6em 0.6em;
+
+    /* Makes the letters look embossed adding to the skeumorphic theme of the design */
+    text-shadow: .5px .5px 0.9px rgba(0,0,0, .4);
+
+    z-index: -1;
+}
+
+/* For the numbering in the range's design - doesn't show on firefox but shows on chrome and opera */
+
+
+input[type="range"].trueFalse::before {
+    position: absolute;
+    text-align: center;
+    color: #c4c4c4;
+    font-size: 1em;
+
+    /*Serves to spread the letters to fit the whole width of the track */
+    letter-spacing: 2.5vw;
+
+    top: 0; bottom: 0;
+    left: 0; right: 0;
+    padding: 1.6em 0.6em 1.6em 0.6em;
+    
+    
+
+    /* Makes the letters look embossed adding to the skeumorphic theme of the design */
+    text-shadow: .5px .5px 0.9px rgba(0,0,0, .4);
+
+    z-index: -1;
+}
+
+
+
+.speech {
+  display: flex;
+  
+}
+
+
+
 .box {
+  flex-grow: 1;
   border: 2px solid grey;
   height: 35px;
   border-radius: 8px;
 }
+
 .smallText {
   font-size: 8px;
   text-align: end;
 }
+
+.btn {
+
+    background-color: #fd8421;
+    border-radius: 20px;
+    box-shadow: 
+                inset .8em .8em 5px -8px rgba(255, 255, 255, .4),
+                inset -.4em -.4em 10px -3px rgba(0,0,0,.2),
+                0.7em 0.5em 7px -0.5em rgba(0,0,0,.4);
+    cursor: pointer;
+    border: 0;
+
+  position: relative;
+  display: block;
+  margin: 30px auto;
+  padding: 12px 24px;
+  overflow: hidden;
+  border-width: 0;
+  outline: none;
+
+  color: #ecf0f1;
+  
+  transition: background-color .3s;
+}
+
+.speech button { 
+  display: inline-block;
+
+    font-size: 36px;
+    border: none;
+    margin: 0;
+    text-decoration: none;
+    background: transparent;
+    color: #ffffff;
+    font-family: sans-serif;
+    font-size: 1rem;
+    cursor: pointer;
+    text-align: center;
+    transition: background 250ms ease-in-out, 
+                transform 150ms ease;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+
+}
+
+.btn:hover, .btn:focus {
+  background-color: #27ae60;
+}
+
+
+
+
+
 </style>
